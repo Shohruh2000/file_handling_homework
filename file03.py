8c4c811174f0dc0618b970809ddc2f8982691a20
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-11 07:05:45 +0500

    file03.py

--- file03.py
+++ file03.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    data.split()
+    list1 = []
+    for i in data:
+        for j in range(len(i)):
+            if str(i[j]).isdigit():
+                list1 +=i[j]
+    return list1
     
 # Read data from file
+f = open("txt_file/data03.txt").read()
+print(main(f))
+
