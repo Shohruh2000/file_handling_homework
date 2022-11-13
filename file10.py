fbffb75cb8de8cc1d9cfdf497b4a23b0ea9fcf7a
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-13 16:26:26 +0500

    file10.py

--- file10.py
+++ file10.py
@@ -6,5 +6,21 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    data.split("\n")
+   
+    ls = []
+    ls1 = []
+    ls = data.split("\n")
+    for i in ls:
+        ls1.append(len(i))
+        max = ls1[0]
+        n = len(ls1)
+        j = 0
+    for j in range(n):
+        if ls1[j] > max:
+            max = ls1[j]
+    return max
 
 # Read data from file
+f = open("txt_file/data10.txt").read()
+print(main(f))
