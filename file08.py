cb2ad047272f4aef905adbf26aaf3f4798896327
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-13 20:13:30 +0500

    file08.py

--- file08.py
+++ file08.py
@@ -6,5 +6,20 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    
+    data.split("\n: :.")
+    ls = []
+    for i in data:
+        for j in range(len(i)):
+            if str(i[j]).isdigit():
+                ls +=i[j]
+    max = ls[0]
+    k = 0
+    for i in range(len(ls)):
+        if ls[i]>max:
+            max = ls[i]
+    return max
 
 # Read data from file
+f = open("txt_file/data08.txt").read()
+print(main(f))
