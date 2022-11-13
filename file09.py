e7acb3787236f357d98cc57e092e1e52f2a487a7
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-13 18:13:23 +0500

    file09.py

--- file09.py
+++ file09.py
@@ -6,5 +6,21 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    data.split("\n")
+    list1 = []
+    for i in data.split("n"):
+        for j in range(len(i)):
+            if str(i[j]).isdigit():
+                list1 +=i[j]
+    n = len(list1)
+    j = 0 
+    min = list1[0]
+    for j in range(n):
+        if list1[j] < min:
+            min = list1[j]
+
+    return min
 
 # Read data from file
+f = open("txt_file/data09.txt").read()
+print(main(f))
